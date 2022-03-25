cf5493ac9bf73f11dbe3f8b7181bf446af47100f
yowai-hito <seow.h.y.95@gmail.com>
2022-03-25 16:38:29 +0800

    Test Draft 1

--- src/test/java/sg/ntu/edu/assessment/AssessmentApplicationTests.java
+++ src/test/java/sg/ntu/edu/assessment/AssessmentApplicationTests.java
@@ -1,13 +1,36 @@
 package sg.ntu.edu.assessment;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObjectBuilder;
+import sg.ntu.edu.assessment.models.Quotation;
+import sg.ntu.edu.assessment.services.QuotationService;
 
 @SpringBootTest
 class AssessmentApplicationTests {
 
 	@Test
 	void contextLoads() {
+		List<String> fruits = new ArrayList<String>()
+		fruits.add("durian");
+		fruits.add("plum");
+		fruits.add("pear");
+
+		Optional<Quotation> optQuote = QuotationService.getQuotations(fruits);
+		Quotation quote = optQuote.get();
+
+		for (String item : fruits){
+			float cost = quote.getQuotation(item);
+			System.out.println("A(n) " + item + " costs $" + cost + ".");
+			System.out.println('\n');
+		}
+		
 	}
 
 }
